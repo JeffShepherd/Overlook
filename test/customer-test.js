e827cf2e468fe2d1ac133fd27f5df85dd40b1cc0
JS <jeff.shep777@gmail.com>
2021-03-06 14:18:58 -0700

    Add tests and class properties for Customer id and name

--- test/customer-test.js
+++ test/customer-test.js
@@ -8,9 +8,35 @@ import {
 
 describe('Customer', function () {
 
+  let customer, customer2;
 
+  beforeEach(function () {
+    customer = new Customer(testUserData[0]);
+    customer2 = new Customer(testUserData[1]);
+  });
 
+  it('should be a function', function () {
+    expect(Customer).to.be.a('function')
+  });
 
+  it('should be an instance of Customer', function () {
+    expect(customer).to.be.an.instanceof(Customer)
+  });
 
+  it('should have an id', function () {
+    expect(customer.id).to.equal(1);
+  });
+
+  it('should be able to have a different id', function () {
+    expect(customer2.id).to.equal(2);
+  });
+
+  it('should have a name', function () {
+    expect(customer.name).to.equal('Leatha Ullrich');
+  });
+
+  it('should be able to have a different name', function () {
+    expect(customer2.name).to.equal('Rocio Schuster');
+  });
 
 });
