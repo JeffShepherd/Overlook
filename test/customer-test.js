7e8bf7b02b6c9ee082324686cae9f399b4d0a980
JS <jeff.shep777@gmail.com>
2021-03-06 14:04:42 -0700

    Add test user data and relevant imports/exports

---
+++ test/customer-test.js
@@ -0,0 +1,16 @@
+import {
+  expect
+} from 'chai';
+import Customer from '../src/Customer';
+import {
+  testUserData
+} from './test-data';
+
+describe('Customer', function () {
+
+
+
+
+
+
+});
---
+++ test/test-data.js
@@ -0,0 +1,13 @@
+export const testUserData = [{
+  "id": 1,
+  "name": "Leatha Ullrich"
+}, {
+  "id": 2,
+  "name": "Rocio Schuster"
+}, {
+  "id": 3,
+  "name": "Kelvin Schiller"
+}, {
+  "id": 4,
+  "name": "Kennedi Emard"
+}]
