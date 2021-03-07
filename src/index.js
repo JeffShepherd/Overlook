742fa7a6bd37920bf1f478196bbcc8ab3039c173
JS <jeff.shep777@gmail.com>
2021-03-07 11:15:15 -0700

    Add function to display total cost

--- src/index.js
+++ src/index.js
@@ -9,16 +9,35 @@ import {
 import Customer from './Customer';
 import Hotel from './Hotel';
 
+
+//query selectors
+const totalSpent = document.querySelector('#totalSpent');
+
+
+//global variables
 let hotel, currentUser;
 
+
+//main startup function
 function startApplication() {
   Promise.all([getData('rooms'), getData('bookings'), getData('customers/1')]) //3rd request hardcoded for now
     .then(values => {
-      hotel = new Hotel(values[0], values[1])
+      hotel = new Hotel(values[0].rooms, values[1].bookings)
       currentUser = new Customer(values[2])
       //dom display function kick-off here
+      populatePage()
     })
 }
+
+function populatePage() {
+  displayTotalCost()
+}
+
+function displayTotalCost() {
+  const cost = hotel.calculateTotalCost(currentUser.id);
+  totalSpent.innerText = `Total spent on rooms: $${cost.toFixed(2)}`;
+}
+
 
 
 window.addEventListener('load', startApplication)
